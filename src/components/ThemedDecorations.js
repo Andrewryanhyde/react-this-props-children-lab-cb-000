ecce0a487c76af3f046c97aa4e342de1a4255e88
Andrew Hyde <andrewryanhyde@hotmail.co.uk>
2018-10-25 09:18:40 +0000

    automatically backed up by learn

--- src/components/ThemedDecorations.js
+++ src/components/ThemedDecorations.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types'
 
 export default class ThemedDecorations extends React.component{
   render() {
+    
     const childrenProp = React.Children.map(this.props.children, child => {
      return React.cloneElement(child, {
        className: this.props.theme
